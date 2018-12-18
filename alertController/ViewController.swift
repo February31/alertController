95071facb964d3d6a1f7aa64c3b5b551427bcae2
Wu <wu@wudemacbook-air.local>
2018-12-18 09:37:09 +0800

    hkjh

--- alertController/ViewController.swift
+++ alertController/ViewController.swift
@@ -15,6 +15,47 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func actionSheet(_ sender: Any) {
+        let alert=UIAlertController(title: "action sheet", message: "please choose color", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "red", style: .default, handler: {(action) in
+            self.view.backgroundColor=UIColor.red
+        }))
+        alert.addAction(UIAlertAction(title: "green", style: .default, handler: {(action) in
+            self.view.backgroundColor=UIColor.green
+        }))
+        alert.addAction(UIAlertAction(title: "blue", style: .destructive, handler: {(action) in
+            self.view.backgroundColor=UIColor.blue
+        }))
+        alert.addAction(UIAlertAction(title: "white", style: .cancel, handler: {(action) in
+            self.view.backgroundColor=UIColor.white
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func alert(_ sender: Any) {
+        let alert=UIAlertController(title: "Alert", message: "login message", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "login", style: .default, handler: {(action) in
+            guard let username=alert.textFields?.first?.text,let password=alert.textFields?.last?.text else {
+                return
+            }
+            print("username \(username) password \(password)")
+        }))
+        alert.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: {(action) in
+            
+        }))
+        alert.addTextField{
+            (textField) in
+            textField.placeholder="your user name?"
+        }
+        alert.addTextField{
+            (textField) in
+            textField.placeholder="your password?"
+            textField.isSecureTextEntry=true
+        }
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
+
 
